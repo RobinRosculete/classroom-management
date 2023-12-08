a40deda09fe0ef4b7f3bf8e5d41b428bd157466a
RobinRosculete <robin.rosculete.860@my.csun.edu>
2023-12-07 20:13:46 -0800

    Added Post api to post request to database

--- ClassRoomManagementSystemServer/Controllers/RequestController.cs
+++ ClassRoomManagementSystemServer/Controllers/RequestController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassRoomManagementSystemServer.DTOs;
+using ClassRoomManagementSystemServer.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +15,13 @@ namespace ClassRoomManagementSystemServer.Controllers
     [Route("api/[controller]")]
     public class RequestController : Controller
     {
+
+        private readonly MydbContext _db;
+        public RequestController(MydbContext db)
+        {
+            _db = db;
+        }
+
         // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
@@ -26,10 +37,22 @@ namespace ClassRoomManagementSystemServer.Controllers
         }
 
         // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
+        [HttpPost("section-request")]
+        public void PostSectionRequest([FromBody] SectionRequest sectionRequest)
         {
+            // Assuming _db is your DbContext instance
+            var parameters = new[]
+            {
+            new MySqlParameter("@DayOfWeek", sectionRequest.Day),
+            new MySqlParameter("@StartTime", sectionRequest.Starttime),
+            new MySqlParameter("@EndTime", sectionRequest.EndTime),
+            new MySqlParameter("@CourseCode", sectionRequest.CourseTitle),
+            new MySqlParameter("@Year", sectionRequest.Year),
+            new MySqlParameter("@Semester", sectionRequest.Semester),
+            new MySqlParameter("@Department", sectionRequest.DepartmentName)
+        };
 
+            _db.Database.ExecuteSqlRaw("CALL PerformSectionAndRequestAssignment(@DayOfWeek, @StartTime, @EndTime, @CourseCode, @Year, @Semester, @Department)", parameters);
 
         }
 
---
+++ ClassRoomManagementSystemServer/DTOs/SectionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ClassRoomManagementSystemServer.DTOs;
+
+	public class SectionRequest
+	{
+    public string Day { get; set; } = null!;
+    public TimeOnly Starttime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public string CourseTitle { get; set; } = null!;
+    public short Year { get; set; }
+    public string Semester { get; set; } = null!;
+    public string DepartmentName { get; set; } = null!;
+
+}
+
+
